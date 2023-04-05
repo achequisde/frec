b976eb4ca7200bb7c9629871f20d9f605de44250
achequisde <therealhjrp@gmail.com>
2023-04-04 20:09:45 -0400

    Add step visualizer init function

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,10 @@
 import Frec from "./frec";
+import { Step } from "./visualizers";
 
 const audioElem = document.querySelector("audio") as HTMLAudioElement;
 const container = document.querySelector("#container") as HTMLElement;
 
-const frec = Frec.createBar(container, audioElem);
-frec.startTimer();
+// const frec = Frec.createBar(container, audioElem);
+// frec.startTimer();
+
+let step = new Step(container);
